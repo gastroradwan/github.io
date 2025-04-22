49bd91b9b06cc522d81d4afdd6d888ddbb997c71
gastroradwan <info@gastroradwan.eu>
2025-04-22 20:29:02 +0200

    Create questionsEZ1.js

---
+++ data/questionsEZ1.js
@@ -0,0 +1,42 @@
+export const questionsEZ1 = [
+  { q: "Upravuje zákon č. 250/2021 Sb. požadavky na bezpečnost provozu VTZ?", options: ["Ano", "Ne", "Pouze u plynových zařízení"], a: "Ano" },
+  { q: "Kdo vykonává státní správu v oblasti bezpečnosti VTZ?", options: ["TIČR", "MPSV", "MPO"], a: "MPSV" },
+  { q: "Jaká je platnost oprávnění k odborné činnosti na VTZ?", options: ["2 roky", "5 let", "Na dobu neurčitou"], a: "5 let" },
+  { q: "Musí být průvodní dokumentace VTZ k dispozici po celou dobu provozu?", options: ["Ano", "Pouze při uvedení do provozu", "Ne"], a: "Ano" },
+  { q: "Kdo je nadřízeným správním orgánem TIČR?", options: ["Česká obchodní inspekce", "Státní úřad inspekce práce", "MPSV"], a: "MPSV" },
+  { q: "Jak se určuje stav VTZ při kontrolách, zkouškách nebo revizích?", options: ["Podle výrobního čísla zařízení", "Podle subjektivního posouzení revizního technika", "Podle skutečného stavu při kontrole"], a: "Podle skutečného stavu při kontrole" },
+  { q: "Jak se určuje základní technologická funkce VTZ?", options: ["Z dokumentace výrobce", "Z provozního předpisu", "Z návrhu provozovatele a výrobce"], a: "Z návrhu provozovatele a výrobce" },
+  { q: "Jaký doklad vydává TIČR na vyžádání k ověření bezpečnosti provozu zařízení?", options: ["Protokol o kontrole BOZP", "Stanovisko", "Osvědčení o odborné způsobilosti"], a: "Stanovisko" },
+  { q: "Co je odborně způsobilá osoba dle zákona č. 250/2021 Sb.?", options: ["Každý zaměstnanec provozovatele", "Revizní technik s osvědčením", "Technik s vyučením v oboru"], a: "Revizní technik s osvědčením" },
+  { q: "Co znamená zkratka TIČR?", options: ["Technický inspektorát ČR", "Technická inspekce České republiky", "Technologický institut ČR"], a: "Technická inspekce České republiky" },
+
+  { q: "Jaký je účel zákona č. 250/2021 Sb.?", options: ["Zabezpečení IT infrastruktury", "Bezpečnost VTZ a ochrana zdraví při práci", "Správa energetické účinnosti"], a: "Bezpečnost VTZ a ochrana zdraví při práci" },
+  { q: "Kdo vydává oprávnění právnické osobě pro činnost na VTZ?", options: ["Krajský úřad", "TIČR", "Ministerstvo zdravotnictví"], a: "TIČR" },
+  { q: "Jak se označuje prostor se zvýšeným rizikem ohrožení života?", options: ["BNV", "OZV", "ZORL"], a: "BNV" },
+  { q: "Kdo stanovuje pravidla pro vydání osvědčení?", options: ["TIČR", "Státní úřad inspekce práce", "MPSV"], a: "TIČR" },
+  { q: "Jak se provádí prokazování odborné způsobilosti?", options: ["Zápisem do provozního deníku", "Ústní zkouškou před komisí", "Zkouškou podle NV č. 194/2022 Sb."], a: "Zkouškou podle NV č. 194/2022 Sb." },
+
+  { q: "Jaká je platnost osvědčení o odborné způsobilosti?", options: ["1 rok", "5 let", "doživotně"], a: "5 let" },
+  { q: "Kdo provádí dozor nad stanovenými výrobky?", options: ["TIČR", "ČOI", "Státní zkušebna"], a: "ČOI" },
+  { q: "Jaký zákon upravuje technické požadavky na výrobky?", options: ["Zákon 22/1997 Sb.", "Zákon 90/2016 Sb.", "Zákon 378/2001 Sb."], a: "Zákon 22/1997 Sb." },
+  { q: "Co znamená značka CE na výrobku?", options: ["Výtvor EU", "Splnění požadavků předpisů EU", "Záruka výrobce"], a: "Splnění požadavků předpisů EU" },
+  { q: "Kdo vydává harmonizované technické normy?", options: ["ČMI", "ÚNMZ", "TIČR"], a: "ÚNMZ" },
+
+  { q: "Kdy se sepíše záznam o úrazu?", options: ["Při každém zranění", "Při pracovním úrazu s dobou pracovní neschopnosti", "Pouze při smrtelném úrazu"], a: "Při pracovním úrazu s dobou pracovní neschopnosti" },
+  { q: "Co je prevence rizik?", options: ["Zajištění bezpečného odstupu", "Souhrn opatření k předcházení úrazům", "Periodické školení zaměstnanců"], a: "Souhrn opatření k předcházení úrazům" },
+  { q: "Kdo je správcem evidence odborně způsobilých osob?", options: ["MPSV", "TIČR", "ČOI"], a: "TIČR" },
+  { q: "Co je technický předpis dle zákona č. 22/1997 Sb.?", options: ["Interní směrnice firmy", "Vyhláška TIČR", "Závazný předpis s technickými požadavky"], a: "Závazný předpis s technickými požadavky" },
+  { q: "Kdo rozhoduje o odejmutí oprávnění k výkonu činností?", options: ["TIČR", "Oblastní inspektorát práce", "MPSV"], a: "Oblastní inspektorát práce" },
+
+  { q: "Co znamená značka CCZ na výrobku?", options: ["Výrobek není určen pro export", "Výrobek splňuje české technické normy", "Značka kvality"], a: "Výrobek splňuje české technické normy" },
+  { q: "Je možné použít současně značku CE a CCZ?", options: ["Ano", "Ne", "Pouze u elektrozařízení"], a: "Ne" },
+  { q: "Kdy je výrobek považován za stanovený?", options: ["Když je uveden v příloze NV", "Pokud jej vyrábí držitel certifikátu", "Když je elektrický"], a: "Když je uveden v příloze NV" },
+  { q: "Které výrobky podléhají povinnosti posouzení shody?", options: ["Pouze zdravotnické prostředky", "Výrobky se zvýšeným rizikem ohrožení zájmu", "Jen dovážené výrobky"], a: "Výrobky se zvýšeným rizikem ohrožení zájmu" },
+  { q: "Kde je definována ochrana zdraví při práci?", options: ["Zákoník práce", "Zákon o ochraně spotřebitele", "Zákon o bezpečnosti práce"], a: "Zákoník práce" },
+
+  { q: "Který právní předpis upravuje používání ochranných pracovních prostředků?", options: ["NV č. 390/2021 Sb.", "Zákon č. 22/1997 Sb.", "NV č. 176/2008 Sb."], a: "NV č. 390/2021 Sb." },
+  { q: "Kdy je nutné instalovat ochranu před bleskem?", options: ["Pouze na nemocnicích", "Při riziku úderu blesku", "Vždy při použití venkovního osvětlení"], a: "Při riziku úderu blesku" },
+  { q: "Jaká je minimální vzdálenost ochranného pásma podzemního vedení do 1 kV?", options: ["0,5 m", "1 m", "2 m"], a: "1 m" },
+  { q: "Kdo určuje rozsah kontroly zařízení bez průvodní dokumentace?", options: ["Revizní technik", "Provozovatel", "Oprávněná osoba dle zákona"], a: "Oprávněná osoba dle zákona" },
+  { q: "Co je cílem státního zkušebnictví?", options: ["Ověřit bezpečnost výrobků", "Zajistit nízkou cenu výrobků", "Stanovit harmonogram školení"], a: "Ověřit bezpečnost výrobků" }
+];
