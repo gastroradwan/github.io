21e8fb9f3e2e8ff2192c69551512510f52753a58
gastroradwan <info@gastroradwan.eu>
2025-09-14 15:47:54 +0200

    Delete zarizeni directory

--- zarizeni/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id = basename($_SERVER['REQUEST_URI']);  // GR-0000177
-$base = "https://script.google.com/macros/s/AKfycbx_6FbDzAxDj7Wp1f_oydlN4-V3tEM-5tV2d7VIs4mIukBBYhMCa0BH5q7q1gLGarpF6A/exec";
-header("Location: {$base}?id={$id}");
-exit;
-?>
